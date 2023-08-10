574aef61ca5d53fc52f98bfb6e5f3be43b993a16
Manyata <138794188+Manyata39@users.noreply.github.com>
2023-08-11 00:33:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function tothird(){
+    document.getElementById("lift").classList.add("classname");
+    //tothirdreset();
+}
+// function tothirdreset(){
+//     document.getElementById("lift").classList.remove("classname");
+// }
+
+function tosecond(){
+    document.getElementById("lift").classList.add("classname1");
+}
+
+function tofirst(){
+    document.getElementById("lift").classList.add("classname2");
+}
